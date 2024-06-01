338610d4b79b36e8f302c02696dcefb0c78fc3f2
dev-Beltramin <paulobeltramin.27@gmail.com>
2024-05-31 22:15:15 -0300

    alterado toda a funcionalidade do projeto para o redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import { useEffect, useState } from 'react'
 import Header from './components/Header'
 import Produtos from './containers/Produtos'
 
 import { GlobalStyle } from './styles'
-import { Provider } from 'react-redux'
 import { Store } from './store'
+import { Provider } from 'react-redux'
 
 export type Game = {
   id: number
@@ -17,22 +16,14 @@ export type Game = {
 }
 
 function App() {
-  const [games, setGames] = useState<Game[]>([])
-
-  useEffect(() => {
-    fetch('http://localhost:4000/produtos')
-      .then((res) => res.json())
-      .then((res) => setGames(res))
-  }, [])
-
   return (
     <Provider store={Store}>
       <GlobalStyle />
       <div className="container">
-        <Header itensNoCarrinho={carrinho} />
-        <Produtos jogos={games} adicionarAoCarrinho={adicionarAoCarrinho} />
+        <Header />
+        <Produtos />
       </div>
-    <Provider/>
+    </Provider>
   )
 }
 
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit'
 
 import CarReducer from '../reducers/car'
+import api from '../services/api'
 
 export const Store = configureStore({
   reducer: {
-    carrinho: CarReducer
-  }
+    carrinho: CarReducer,
+    [api.reducerPath]: api.reducer
+  },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware)
 })
 
 export type RootReducer = ReturnType<typeof Store.getState>
